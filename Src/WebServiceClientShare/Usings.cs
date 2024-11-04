a3a253aa6ba0fa8dae48ea979ab9e5c6f49f334a
Ralf Beckers <ralf.beckers@elektrobit.com>
2024-11-04 13:11:08 +0100

    Update

--- Src/WebServiceClientShare/Usings.cs
+++ Src/WebServiceClientShare/Usings.cs
@@ -5,6 +5,7 @@ global using System.Linq;
 global using System.Net;
 global using System.Net.Http.Headers;
 global using System.Net.Http.Json;
+global using System.Runtime.CompilerServices;
 global using System.Runtime.Serialization;
 global using System.Security.Cryptography;
 global using System.Text;
@@ -12,6 +13,7 @@ global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Threading.Tasks;
 global using System.Xml.Linq;
+
 
 global using WebServiceClient.Authenticator;
 global using WebServiceClient.JsonConverters;

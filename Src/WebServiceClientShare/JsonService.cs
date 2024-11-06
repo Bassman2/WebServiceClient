c58d8843eee5e93f506c1f7132a1117a185416be
Ralf Beckers <ralf.beckers@elektrobit.com>
2024-11-06 21:06:57 +0100

    Update

--- Src/WebServiceClientShare/JsonService.cs
+++ Src/WebServiceClientShare/JsonService.cs
@@ -84,14 +84,14 @@ public class JsonService : WebService
         return await ReadFromJsonAsync<T>(response, cancellationToken);
     }
 
-    protected T? PutAsJson<T>(string requestUri, T obj, [CallerMemberName] string memberName = "")
+    protected T2? PutAsJson<T1, T2 >(string requestUri, T1 obj, [CallerMemberName] string memberName = "")
     {
         ArgumentRequestUriException.ThrowIfNullOrWhiteSpace(requestUri, nameof(requestUri));
         ArgumentNullException.ThrowIfNull(obj, nameof(obj));
         WebServiceException.ThrowIfNullOrNotConnected(this);
 
 #if NET8_0_OR_GREATER
-        JsonTypeInfo<T> jsonTypeInfo = (JsonTypeInfo<T>)context.GetTypeInfo(typeof(T))!;
+        JsonTypeInfo<T1> jsonTypeInfo = (JsonTypeInfo<T1>)context.GetTypeInfo(typeof(T1))!;
         using HttpResponseMessage response = client!.PutAsJsonAsync(requestUri, obj, jsonTypeInfo).Result;
 #else
         using HttpResponseMessage response = client!.PutAsJsonAsync(requestUri, obj, jsonSerializerOptions).Result;
@@ -101,17 +101,17 @@ public class JsonService : WebService
             ErrorHandling(response, memberName);
         }
 
-        return ReadFromJson<T>(response);
+        return ReadFromJson<T2>(response);
     }
 
-    protected async Task<T?> PutAsJsonAsync<T>(string requestUri, T obj, CancellationToken cancellationToken = default, [CallerMemberName] string memberName = "")
+    protected async Task<T2?> PutAsJsonAsync<T1, T2>(string requestUri, T1 obj, CancellationToken cancellationToken = default, [CallerMemberName] string memberName = "")
     {
         ArgumentRequestUriException.ThrowIfNullOrWhiteSpace(requestUri, nameof(requestUri));
         ArgumentNullException.ThrowIfNull(obj, nameof(obj));
         WebServiceException.ThrowIfNullOrNotConnected(this);
 
 #if NET8_0_OR_GREATER
-        JsonTypeInfo<T> jsonTypeInfo = (JsonTypeInfo<T>)context.GetTypeInfo(typeof(T))!;
+        JsonTypeInfo<T1> jsonTypeInfo = (JsonTypeInfo<T1>)context.GetTypeInfo(typeof(T1))!;
         using HttpResponseMessage response = await client!.PutAsJsonAsync(requestUri, obj, jsonTypeInfo);
 #else
         using HttpResponseMessage response = await client!.PutAsJsonAsync(requestUri, obj, jsonSerializerOptions);
@@ -121,7 +121,7 @@ public class JsonService : WebService
             ErrorHandling(response, memberName);
         }
 
-        return await ReadFromJsonAsync<T>(response);
+        return await ReadFromJsonAsync<T2>(response);
     }
 
     protected void PostAsJson<T>(string requestUri, T obj, [CallerMemberName] string memberName = "")
@@ -194,16 +194,81 @@ public class JsonService : WebService
         return stream;
     }
 
-    protected void Delete(string requestUri, [CallerMemberName] string memberName = "")
+    protected void Download(Uri requestUri, string filePath, [CallerMemberName] string memberName = "")
+    {
+        //ArgumentRequestUriException.ThrowIfNullOrWhiteSpace(requestUri, nameof(requestUri));
+        WebServiceException.ThrowIfNullOrNotConnected(this);
+
+        using HttpResponseMessage response = client!.GetAsync(requestUri).Result;
+        string str = response.Content.ReadAsStringAsync().Result;
+        if (!response.IsSuccessStatusCode)
+        {
+            ErrorHandling(response, memberName);
+        }
+        using var file = File.Create(filePath);
+        response.Content.CopyToAsync(file).Wait();
+    }
+
+    protected void Download(string requestUri, string filePath, [CallerMemberName] string memberName = "")
+    {
+        ArgumentRequestUriException.ThrowIfNullOrWhiteSpace(requestUri, nameof(requestUri));
+        WebServiceException.ThrowIfNullOrNotConnected(this);
+
+        using HttpResponseMessage response = client!.GetAsync(requestUri).Result;
+        string str = response.Content.ReadAsStringAsync().Result;
+        if (!response.IsSuccessStatusCode)
+        {
+            ErrorHandling(response, memberName);
+        }
+        using var file = File.Create(filePath);
+        response.Content.CopyToAsync(file).Wait();
+    }
+
+    protected async Task DownloadAsync(Uri requestUri, string filePath, [CallerMemberName] string memberName = "")
+    {
+        //ArgumentRequestUriException.ThrowIfNullOrWhiteSpace(requestUri, nameof(requestUri));
+        WebServiceException.ThrowIfNullOrNotConnected(this);
+
+        using HttpResponseMessage response = await client!.GetAsync(requestUri);
+        string str = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            ErrorHandling(response, memberName);
+        }
+        using var file = File.Create(filePath);
+        await response.Content.CopyToAsync(file);
+    }
+
+    protected async Task DownloadAsync(string requestUri, string filePath, [CallerMemberName] string memberName = "")
+    {
+        ArgumentRequestUriException.ThrowIfNullOrWhiteSpace(requestUri, nameof(requestUri));
+        WebServiceException.ThrowIfNullOrNotConnected(this);
+
+        using HttpResponseMessage response = await client!.GetAsync(requestUri);
+        string str = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            ErrorHandling(response, memberName);
+        }
+        using var file = File.Create(filePath);
+        await response.Content.CopyToAsync(file);
+    }
+
+    protected bool Delete(string requestUri, [CallerMemberName] string memberName = "")
     {
         ArgumentRequestUriException.ThrowIfNullOrWhiteSpace(requestUri, nameof(requestUri));
         WebServiceException.ThrowIfNullOrNotConnected(this);
 
         using HttpResponseMessage response = client!.DeleteAsync(requestUri).Result;
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return false;
+        }
         if (!response.IsSuccessStatusCode)
         {
-            ErrorHandling(response, memberName);
+             ErrorHandling(response, memberName);
         }
+        return true;
     }
 
     protected async Task DeleteAsync(string requestUri, [CallerMemberName] string memberName = "")

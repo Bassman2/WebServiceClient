c07281eee9b9ae4f8d3cb508bc20c2fec0bbfd4b
Ralf Beckers <Ralf.Beckers@hotmail.com>
2024-11-24 18:36:04 +0100

    add Xml Serialization

---
+++ Src/WebServiceClientShare/XSerializer/IXSerializable.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServiceClient.XSerializer;
+
+// default XML Serializer is not trimmable and AOT compatible
+
+public interface IXSerializable
+{
+    void ReadX(XElement elm);
+    //void WriteX(XElement elm);
+}
+

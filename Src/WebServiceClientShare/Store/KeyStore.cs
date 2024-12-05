7da4387f9e11f7ebdc28cc1f1179a2536f53d36f
kersRalf Beckers <ralf.beckers@elektrobit.com>
2024-12-05 17:09:22 +0100

    Update

--- Src/WebServiceClientShare/Store/KeyStore.cs
+++ Src/WebServiceClientShare/Store/KeyStore.cs
@@ -3,15 +3,18 @@
 // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation
 
 
-[JsonSourceGenerationOptions(WriteIndented = true)]
-[JsonSerializable(typeof(KeyStore))]
-[JsonSerializable(typeof(KeyItem))]
-[JsonSerializable(typeof(List<KeyItem>))]
+[JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull, WriteIndented = true, AllowTrailingCommas = true)]
+[JsonSerializable(typeof(Dictionary<string, KeyStore>))]
 internal partial class KeyStoreContext : JsonSerializerContext
 { }
 
-public class KeyItem
+public class KeyStore
 {
+    private static readonly string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "KeyStore", "KeyStore.json");
+
+    private static Dictionary<string, KeyStore>? items = null;
+
+
     [JsonPropertyName("host")]
     public string? Host { get; set; }
 
@@ -23,24 +26,36 @@ public class KeyItem
 
     [JsonPropertyName("token")]
     public string? Token { get; set; }
-}
 
-public class KeyStore
-{
-    private static readonly string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "KeyStore", "KeyStore.json");
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
 
+    [JsonPropertyName("email")]
+    public string? Email { get; set; }
 
-    public required List<KeyItem> Items { get; init; }
-
-    public static KeyStore? Load()
+    public static KeyStore? Key(string name)
     {
-        using Stream stream = File.OpenRead(path);
-        return new KeyStore() { Items = JsonSerializer.Deserialize<List<KeyItem>>(stream, KeyStoreContext.Default.ListKeyItem) ?? [] };
-    }
-
-    public void Save()
-    {
-        using Stream stream = File.Create(path);
-        JsonSerializer.Serialize<List<KeyItem>>(stream, Items, KeyStoreContext.Default.ListKeyItem);
+        if (items == null)
+        {
+            if (File.Exists(path))
+            {
+                // load existing file
+                using Stream stream = File.OpenRead(path);
+                items = JsonSerializer.Deserialize<Dictionary<string, KeyStore>>(stream, KeyStoreContext.Default.DictionaryStringKeyStore);
+            }
+            else
+            {
+                // create demo file if not exists
+                items = new()
+                {
+                    { "jira", new KeyStore() { Host = "https://www.atlassian.com/", Login = "mm", Password = "1234", Token = "", Name = "Max Mustermann", Email = "Max.Mustermann@web.de" } },
+                    { "github", new KeyStore() { Host = "https://github.com/", Login = "mm", Password = "1234", Token = "", Name = "Max Mustermann", Email = "Max.Mustermann@web.de" } }
+                };
+                Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+                using Stream stream = File.Create(path);
+                JsonSerializer.Serialize<Dictionary<string, KeyStore>>(stream, items, KeyStoreContext.Default.DictionaryStringKeyStore);
+            }
+        }
+        return items?.GetValueOrDefault(name);
     }
 }

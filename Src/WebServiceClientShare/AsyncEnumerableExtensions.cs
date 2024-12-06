2dafad8c94b7f0dbe1f136b783997bb504fba5c4
Ralf Beckers <Ralf.Beckers@hotmail.com>
2024-12-06 15:07:43 +0100

    Update

--- Src/WebServiceClientShare/AsyncEnumerableExtensions.cs
+++ Src/WebServiceClientShare/AsyncEnumerableExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace WebServiceClient;
+﻿using System.Collections.Concurrent;
+
+namespace WebServiceClient;
 
 public static class AsyncEnumerableExtensions
 {
@@ -11,6 +13,29 @@ public static class AsyncEnumerableExtensions
         }
         return results;
     }
+
+    public static IEnumerable<T> ToEnumerable<T>(this IAsyncEnumerable<T> asyncEnumerable)
+    {
+        var list = new BlockingCollection<T>();
+
+        async Task AsyncIterate()
+        {
+            await foreach (var item in asyncEnumerable)
+            {
+                list.Add(item);
+            }
+
+            list.CompleteAdding();
+        }
+
+        _ = AsyncIterate();
+
+        return list.GetConsumingEnumerable();
+    }
+
+    public static List<T> ToList<T>(this IAsyncEnumerable<T> asyncEnumerable)
+        => asyncEnumerable.ToEnumerable().ToList();
+
 
     //public static async IAsyncEnumerable<T2> CastList<T1, T2>(this IAsyncEnumerable<T1> items, [EnumeratorCancellation] CancellationToken cancellationToken = default)
     //{

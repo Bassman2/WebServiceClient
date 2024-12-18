8eb7becc5b80f5af05669f02dd069463f978e5ce
Ralf Beckers <Ralf.Beckers@hotmail.com>
2024-12-18 22:00:12 +0100

    Update

--- Src/WebServiceClientShare/Authenticator/ApiKeyAuthenticator.cs
+++ Src/WebServiceClientShare/Authenticator/ApiKeyAuthenticator.cs
@@ -4,6 +4,7 @@ internal class ApiKeyAuthenticator(string name, string value) : IAuthenticator
 {
     public void Authenticate(WebService service, HttpClient client)
     {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", value);
         client.DefaultRequestHeaders.Add(name, value);
     }
 }

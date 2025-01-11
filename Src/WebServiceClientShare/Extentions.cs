38a8c8ebff228b22418ec1d34b1bc07f318f5187
Ralf Beckers <ralf.beckers@hotmail.com>
2025-01-11 16:10:27 +0100

    Update

--- Src/WebServiceClientShare/Extentions.cs
+++ Src/WebServiceClientShare/Extentions.cs
@@ -187,12 +187,12 @@ internal static class Extentions
 
     #region Facade
 
-    public static T? Facade<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(this object? value) where T : class
-    {
-        if (value is null) { return null; }
+    //public static T? Facade<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(this object? value) where T : class
+    //{
+    //    if (value is null) { return null; }
 
-        return (T?)Activator.CreateInstance(typeof(T), value);
-    }
+    //    return (T?)Activator.CreateInstance(typeof(T), value);
+    //}
 
     //public static T2? Facade<T1, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T2>(this T1? value) where T1 : class where T2 : class
     //{
@@ -201,12 +201,12 @@ internal static class Extentions
     //    return (T2?)Activator.CreateInstance(typeof(T2), value);
     //}
 
-    public static IEnumerable<T2>? Facade<T1, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T2>(this IEnumerable<T1>? values) where T1 : class where T2 : class
-    {
-        if (values is null) { return null; }
+    //public static IEnumerable<T2>? Facade<T1, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T2>(this IEnumerable<T1>? values) where T1 : class where T2 : class
+    //{
+    //    if (values is null) { return null; }
 
-        return values.Select(item => ((T2?)Activator.CreateInstance(typeof(T2), item))!);
-    }
+    //    return values.Select(item => ((T2?)Activator.CreateInstance(typeof(T2), item))!);
+    //}
 
     #endregion
 

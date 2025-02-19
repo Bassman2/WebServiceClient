2517ece2471759ed4bb3983f4c68d74346f2d87c
Ralf Beckers <ralf.beckers@hotmail.com>
2025-02-19 20:51:41 +0100

    Add namespace to xml serializer

--- Src/WebServiceClientShare/XSerializer/XSerializerExtentions.cs
+++ Src/WebServiceClientShare/XSerializer/XSerializerExtentions.cs
@@ -15,7 +15,7 @@ public static class XSerializableExt
     /// <param name="value">The XML string to deserialize.</param>
     /// <param name="rootName">The root element name of the XML.</param>
     /// <returns>The deserialized object of type T, or default if the value is null.</returns>
-    public static T? XDeserialize<T>(this string? value, string rootName) where T : IXSerializable, new()
+    public static T? XDeserialize<T>(this string? value, XName rootName) where T : IXSerializable, new()
     {
         if (value is null)
         {
@@ -25,6 +25,22 @@ public static class XSerializableExt
         var res = new T();
         var doc = XDocument.Parse(value);
         var elm = doc.Element(rootName)!;
+        if (elm is null) return default;
+        res.ReadX(elm);
+        return res;
+    }
+
+    public static T? XDeserialize<T>(this string? value, string rootElementName,  string namespaceName) where T : IXSerializable, new()
+    {
+        if (value is null)
+        {
+            return default;
+        }
+
+        var res = new T();
+        var doc = XDocument.Parse(value);
+        var elm = doc.Element(XName.Get(rootElementName, namespaceName))!;
+        if (elm is null) return default;
         res.ReadX(elm);
         return res;
     }
@@ -89,15 +105,39 @@ public static class XSerializableExt
         return default;
     }
 
+
+    private static XElement? GetElement(this XElement? elm, XName name)
+    {
+        if (elm is null) return null;
+
+        string namespaceName = string.IsNullOrEmpty(name.NamespaceName) ? elm.Name.NamespaceName : name.NamespaceName;
+        var xName = XName.Get(name.LocalName, namespaceName);
+        return elm.Element(xName);
+    }
+
+    private static IEnumerable<XElement> GetElements(this XElement? elm, XName name)
+    {
+        if (elm is null) return [];
+
+        string namespaceName = string.IsNullOrEmpty(name.NamespaceName) ? elm.Name.NamespaceName : name.NamespaceName;
+        var xName = XName.Get(name.LocalName, namespaceName);
+        return elm.Elements(xName);
+    }
+
+
     /// <summary>
     /// Reads an element value as a string from an XElement.
     /// </summary>
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the element.</param>
     /// <returns>The element value as a string, or null if the element does not exist.</returns>
-    internal static string? ReadElementString(this XElement? elm, string name)
+    internal static string? ReadElementString(this XElement? elm, XName name)
     {
-        return elm?.Element(name)?.Value;
+        return elm.GetElement(name)?.Value;
+        //string namespaceName = string.IsNullOrEmpty(name.NamespaceName) ? elm?.Name.NamespaceName ?? String.Empty : name.NamespaceName;
+        //var xName = XName.Get(name.LocalName, namespaceName);
+        ////name.NamespaceName = name.NamespaceName ?? elm?.Name.NamespaceName;
+        //return elm?.Element(xName)?.Value;
     }
 
     /// <summary>
@@ -106,9 +146,10 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the element.</param>
     /// <returns>The element value as an integer, or null if the element does not exist or cannot be parsed.</returns>
-    internal static int? ReadElementInt(this XElement? elm, string name)
+    internal static int? ReadElementInt(this XElement? elm, XName name)
     {
-        string? value = elm?.Element(name)?.Value;
+//        string? value = elm?.Element(name)?.Value;
+        string? value = elm.GetElement(name)?.Value;
         if (int.TryParse(value, out var val))
         {
             return val;
@@ -122,9 +163,10 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the element.</param>
     /// <returns>The element value as a boolean, or null if the element does not exist or cannot be parsed.</returns>
-    internal static bool? ReadElementBool(this XElement? elm, string name)
+    internal static bool? ReadElementBool(this XElement? elm, XName name)
     {
-        string? value = elm?.Element(name)?.Value;
+        //        string? value = elm?.Element(name)?.Value;
+        string? value = elm.GetElement(name)?.Value;
         if (int.TryParse(value, out var val))
         {
             return val != 0;
@@ -138,9 +180,10 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the element.</param>
     /// <returns>The element value as a DateTime, or null if the element does not exist or cannot be parsed.</returns>
-    internal static DateTime? ReadElementDateTime(this XElement? elm, string name)
+    internal static DateTime? ReadElementDateTime(this XElement? elm, XName name)
     {
-        string? value = elm?.Element(name)?.Value;
+        //string? value = elm?.Element(name)?.Value;
+        string? value = elm.GetElement(name)?.Value;
         if (long.TryParse(value, out var val))
         {
             return begin.AddSeconds(val); //.ToLocalTime();
@@ -155,9 +198,10 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the element.</param>
     /// <returns>The element value as an enum, or default if the element does not exist or cannot be parsed.</returns>
-    internal static T? ReadElementEnum<T>(this XElement? elm, string name) where T : Enum
+    internal static T? ReadElementEnum<T>(this XElement? elm, XName name) where T : Enum
     {
-        string? value = elm?.Element(name)?.Value;
+        //string? value = elm?.Element(name)?.Value;
+        string? value = elm.GetElement(name)?.Value;
         if (int.TryParse(value, out var val))
         {
             return (T)(object)val;
@@ -172,9 +216,10 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the elements.</param>
     /// <returns>A list of enum values, or null if the elements do not exist or cannot be parsed.</returns>
-    internal static List<T>? ReadElementEnums<T>(this XElement? elm, string name) where T : Enum
+    internal static List<T>? ReadElementEnums<T>(this XElement? elm, XName name) where T : Enum
     {
-        return elm?.Elements(name).Select(i => (T)(object)(int.TryParse(i.Value, out int val) ? val : 0))?.ToList();
+//        return elm?.Elements(name).Select(i => (T)(object)(int.TryParse(i.Value, out int val) ? val : 0))?.ToList();
+        return elm.GetElements(name).Select(i => (T)(object)(int.TryParse(i.Value, out int val) ? val : 0))?.ToList();
     }
 
     /// <summary>
@@ -184,13 +229,12 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the element.</param>
     /// <returns>The element value as an object of type T, or default if the element does not exist.</returns>
-    internal static T? ReadElementItem<T>(this XElement? elm, string name) where T : IXSerializable, new()
+    internal static T? ReadElementItem<T>(this XElement? elm, XName name) where T : IXSerializable, new()
     {
-        var child = elm?.Element(name);
-        if (child is null)
-        {
-            return default;
-        }
+//        var child = elm?.Element(name);
+        var child = elm.GetElement(name);
+        if (child is null) return default;
+        
         var item = new T();
         item.ReadX(child);
         return item;
@@ -203,9 +247,10 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the elements.</param>
     /// <returns>A list of string values, or null if the elements do not exist.</returns>
-    internal static List<string>? ReadElementStrings(this XElement? elm, string name)
+    internal static List<string>? ReadElementStrings(this XElement? elm, XName name)
     {
-        return elm?.Elements(name).Select(e => e.Value).ToList();
+        //return elm?.Elements(name).Select(e => e.Value).ToList();
+        return elm.GetElements(name).Select(e => e.Value).ToList();
     }
 
     /// <summary>
@@ -215,9 +260,10 @@ public static class XSerializableExt
     /// <param name="elm">The XElement to read from.</param>
     /// <param name="name">The name of the elements.</param>
     /// <returns>A list of objects of type T, or null if the elements do not exist.</returns>
-    internal static List<T>? ReadElementList<T>(this XElement? elm, string name) where T : IXSerializable, new()
+    internal static List<T>? ReadElementList<T>(this XElement? elm, XName name) where T : IXSerializable, new()
     {
-        return elm?.Elements(name).Select(e => { T i = new(); i.ReadX(e); return i; }).ToList();
+        // return elm?.Elements(name).Select(e => { T i = new(); i.ReadX(e); return i; }).ToList();
+        return elm.GetElements(name).Select(e => { T i = new(); i.ReadX(e); return i; }).ToList();
     }
 
     /// <summary>
@@ -228,8 +274,9 @@ public static class XSerializableExt
     /// <param name="array">The name of the array element.</param>
     /// <param name="name">The name of the elements within the array.</param>
     /// <returns>A list of objects of type T, or null if the elements do not exist.</returns>
-    internal static List<T>? ReadElementList<T>(this XElement? elm, string array, string name) where T : IXSerializable, new()
+    internal static List<T>? ReadElementList<T>(this XElement? elm, XName array, XName name) where T : IXSerializable, new()
     {
-        return elm?.Element(array)?.Elements(name).Select(e => { T i = new(); i.ReadX(e); return i; }).ToList();
+        //return elm?.Element(array)?.Elements(name).Select(e => { T i = new(); i.ReadX(e); return i; }).ToList();
+        return elm.GetElement(array).GetElements(name).Select(e => { T i = new(); i.ReadX(e); return i; }).ToList();
     }
 }

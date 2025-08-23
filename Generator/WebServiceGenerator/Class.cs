1fecafb6e7ffd538e2709566d823aab2d8a159f6
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-08-23 22:14:54 +0200

    Update

--- Generator/WebServiceGenerator/Class.cs
+++ Generator/WebServiceGenerator/Class.cs
@@ -7,30 +7,6 @@ namespace WebServiceGenerator
 {
 	public class Class(INamedTypeSymbol symbol)
 	{
-        public static IEnumerable<Class> GetAllClasses((Compilation Left, ImmutableArray<ClassDeclarationSyntax> Right) tuple)
-        {
-            var (compilation, classes) = tuple;
-
-            foreach (var cla in classes)
-            {
-                if (compilation.GetSemanticModel(cla.SyntaxTree).GetDeclaredSymbol(cla) is INamedTypeSymbol symbol)
-                {
-                    yield return new Class(symbol);
-                }
-            }
-        }
-
-        public static IEnumerable<Class> GetAllClassesWithAttribute((Compilation Left, ImmutableArray<ClassDeclarationSyntax> Right) tuple, string attribute)
-        {
-            foreach (var cla in Class.GetAllClasses(tuple))
-            {
-                if (cla.GetAttribute(attribute) != null)
-                {
-                    yield return cla;
-                }
-            }
-        }
-
         public string Name => symbol.Name;
 
         public string NameSpace => symbol.ToDisplayString();
@@ -69,6 +45,18 @@ namespace WebServiceGenerator
                     yield return new Attribute(attribute);
                 }
             }
+        }
+
+        public bool HasAttribute(string nameWithNamespace)
+        {
+            foreach (var attribute in Attributes)
+            {
+                if (attribute.NameWithNameSpace == nameWithNamespace)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public Attribute? GetAttribute(string nameWithNamespace)
---
+++ Generator/WebServiceGenerator/Generator.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace WebServiceGenerator
+{
+    public class Generator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var provider = context.SyntaxProvider.CreateSyntaxProvider(
+                    predicate: static (node, _) => node is ClassDeclarationSyntax,
+                    transform: static (ctx, _) => (ClassDeclarationSyntax)ctx.Node
+                    ).Where(m => m is not null);
+
+            var compilation = context.CompilationProvider.Combine(provider.Collect());
+
+            context.RegisterSourceOutput(compilation, Excecute);
+        }
+
+        private void Excecute(SourceProductionContext context, (Compilation Left, ImmutableArray<ClassDeclarationSyntax> Right) tuple)
+        {
+            Context = context;
+            Compilation = tuple.Left;
+            Classes = tuple.Right;
+            Excecute();
+        }
+
+        protected SourceProductionContext Context { get; private set; }
+
+        protected Compilation? Compilation { get; private set; }
+
+        protected ImmutableArray<ClassDeclarationSyntax> Classes { get; private set; }
+
+        public virtual void Excecute()
+        { }
+
+        public IEnumerable<Class> GetAllClasses()
+        {
+            foreach (var cla in Classes)
+            {
+                if (Compilation!.GetSemanticModel(cla.SyntaxTree).GetDeclaredSymbol(cla) is INamedTypeSymbol symbol)
+                {
+                    yield return new Class(symbol);
+                }
+            }
+        }
+
+        public IEnumerable<Class> GetAllClassesWithAttribute(string attributeFullName)
+        {
+            foreach (var cla in GetAllClasses())
+            {
+                if (cla.GetAttribute(attributeFullName) != null)
+                {
+                    yield return cla;
+                }
+            }
+        }
+
+        public void AddSource (string hintName, string source) => Context.AddSource(hintName, source);
+    }
+}

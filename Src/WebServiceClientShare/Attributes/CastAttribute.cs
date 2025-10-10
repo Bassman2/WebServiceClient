c5e17cba7689fa4b7adc7249a3f2e9269a9982aa
Ralf Beckers <ralf.beckers@hotmail.com>
2025-10-10 19:47:31 +0200

    Update

---
+++ Src/WebServiceClientShare/Attributes/CastAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServiceClient.Attributes;
+
+/// <summary>
+/// Specifies the class name to be used for casting.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class CastAttribute(string className) : Attribute
+{
+    /// <summary>
+    /// Gets or sets the class name for casting.
+    /// </summary>
+    public string ClassName { get; set; } = className;
+}
